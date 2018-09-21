9c3fb0b1bf0dfb92560e7012e7811f92b3e9dd58
CWJ_iMac <cwj_imac@cwj-imacui-imac.local>
2018-09-21 11:00:39 +0900

    commit 2

---
+++ tesGit/ViewController2.h
@@ -0,0 +1,13 @@
+//
+//  ViewController2.h
+//  tesGit
+//
+//  Created by CWJ_iMac on 2018. 9. 21..
+//  Copyright © 2018년 CWJ_iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController2 : UIViewController
+
+@end
