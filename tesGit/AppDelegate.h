6577cb999611132453bf188d76c72797cc68872c
CWJ_iMac <cwj_imac@cwj-imacui-imac.local>
2018-09-21 09:57:36 +0900

    Initial Commit

---
+++ tesGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tesGit
+//
+//  Created by CWJ_iMac on 2018. 9. 21..
+//  Copyright © 2018년 CWJ_iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tesGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tesGit
+//
+//  Created by CWJ_iMac on 2018. 9. 21..
+//  Copyright © 2018년 CWJ_iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
